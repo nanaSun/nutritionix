104ac424ac1c04bccc325bc992153900188486ba
cherryvenus <sunmeiye@163.com>
2017-06-19 17:57:54 +0800

    api build

--- api.js
+++ api.js
@@ -1,55 +1,71 @@
 "use strict";
 const http = require("http");
 const querystring = require('querystring');
-const apiID="3912a728";
-const apiKey="93348161bf3709746463a237526d180f";
-//https://api.nutritionix.com/v1_1/search
-////https://developer.nutritionix.com/docs/v1_1#/nutritionix_api_v1_1s
+const url = require('url');
+const NutritionixClient = require('nutritionix');
+
+const apiID="";
+const apiKey="";
+const nutritionix = new NutritionixClient({
+    appId: apiID,
+    appKey: apiKey
+});
+function errorHandler(response){
+	response.write(JSON.stringify({"error":1}));
+    response.end();
+}
 function onrequest(request, response){
     let r=null;
     let prettyJson ='';
-    let s={};
-    let opt = {  
-        method: "POST", 
-        path: "https://api.nutritionix.com/v1_1/search",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Content-Length': Buffer.byteLength(s)
-        }
-    };  
-    const req = http.request(options, (res) => {
-  console.log(`STATUS: ${res.statusCode}`);
-  console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-  res.setEncoding('utf8');
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  res.on('end', () => {
-    console.log('No more data in response.');
-  });
-});
-    request.addListener("end", function () {
-        s.phrase="taco";
-        s.apiID=apiID;
-        s.apiKey=apiKey;
-        
-        response.writeHead(200,{"Content-Type":"application/json","Access-Control-Allow-Origin":"http://localhost"});
-        r=
-        if(typeof r.error!=="undefined"&&r.error===1){
-            prettyJson=JSON.stringify(r, null, 4)
-            response.write(prettyJson);
-            response.end();
-        }else{
-            r.then(data => {
-                prettyJson = JSON.stringify(data.jsonBody, null, 4);
-                response.write(prettyJson);
-                response.end();
-            }).catch(e => {
-                prettyJson = JSON.stringify({error:500}, null, 4);
-                response.write(prettyJson);
-                response.end();
-            });
-        }
-    });
+  	let params = url.parse(request.url, true).query;
+    response.writeHead(200,{"Content-Type":"application/json","Access-Control-Allow-Origin":"http://localhost"});	
+    switch(params.action){
+        case 'a': //autocomplete
+           r=nutritionix.autocomplete({q:params.q});
+           break;
+        case 'n': //natural 
+           r=nutritionix.natural(params.q.split(",").join('\n'));
+           break;
+        case 'i': //item
+           r=nutritionix.item({ id: params.q });
+           break;
+        case 'b': //brand
+           r=nutritionix.brand({ id: params.q });
+           break;
+        case 's': 
+           r=nutritionix.search({
+			  q:params.q,
+			  limit: 10,
+			  offset: 0
+			});
+           break;
+        case 'bs': 
+           r=nutritionix.brand_search({
+			    q: params.q,
+			    limit: 10,
+			    offset: 0
+			});
+           break;
+        default: 
+           r={"error":1};
+           break;
+    };
+    if(r.error===1){
+		errorHandler(response);
+    }else{
+    	r.then(function(data){
+    		if(params.action==='a'||params.action==='i'||params.action==='b'){
+				response.write(JSON.stringify(data));
+    		}else{
+	    		response.write(JSON.stringify(data.results));
+	    	}
+	    	response.end();
+	    },function(data){
+	    	errorHandler(response);
+	    }).catch(function(data){
+	    	errorHandler(response);
+	    });
+    }
+	
 }
 http.createServer(onrequest).listen(8888);
